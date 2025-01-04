39417738c30bf68a0a2df9fdbf7747ab89f039ba
Mmehmet Mert Han <mehmetmerthann@gmail.com>
2025-01-04 18:11:56 +0300

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { About } from "./components/About";
+//import { About } from "./components/About";
 import { FAQ } from "./components/FAQ";
-import { Features } from "./components/Features";
+// import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
